2bf9c0b5c8ac74d034f04a77ffbe658e23be8bd9
timzxz <timzxz@qq.com>
2014-12-15 19:25:08 +0800

    basically functional pair and send to real record DB

--- Elevator-Fee/src/elefee/pair.java
+++ Elevator-Fee/src/elefee/pair.java
@@ -5,13 +5,11 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Random;
 public class pair {
 	public static int swift;
 	public static String[] rctime=new String[90000];
-	public static int[] rclevel=new int[90000];
+	public static double[] rcheight=new double[90000];
 	public static int rclength=0;
 	public static String[] dbtime=new String[20000];
 	public static String[] dbuser=new String[20000];
@@ -19,13 +17,24 @@ public class pair {
 	public static String s_buf[]=new String[100];
 	public static int i_buf[][]=new int[100][3];//i_buf[][0]为标志位，i_buf[][1]为入层，i_buf[][2]为出层
 	public static Random r=new Random(100);
+	public static double[][] leveldata=new double[100][2];//leveldata[][0]为下界，leveldata[][1]为下界
 	public static void main(String[] args) throws IOException,SQLException,ClassNotFoundException{
-//		swift=readswift(9);//获取流水号
-//		dblength=getdbinfo();
+		setlevel();
+		swift=readswift(9);//获取流水号
+		dblength=getdbinfo();
 		rclength=getrcinfo();
-//		match();
-//		writeswift(swiftupdate(9,swift));//默认在9号电梯进行。
-		
+		match();
+		writeswift(swiftupdate(9,swift));//默认在9号电梯进行。
+	}
+	public static void setlevel(){
+			leveldata[1][0]=-6.4;leveldata[1][1]=-2.4;
+			leveldata[2][0]=-1.4;leveldata[2][1]=1.4;
+			leveldata[3][0]=3.6;leveldata[1][1]=6.3;
+			leveldata[4][0]=8.1;leveldata[4][1]=10.8;
+			leveldata[5][0]=12.2;leveldata[5][1]=14.8;
+			leveldata[6][0]=16.2;leveldata[6][1]=18.6;
+			leveldata[7][0]=20.6;leveldata[17][1]=22.8;
+			leveldata[8][0]=24.7;leveldata[8][1]=26.7;
 	}
 	public static int getdbinfo() throws SQLException,ClassNotFoundException{//获取临时使用信息
 		int i=0;
@@ -39,12 +48,11 @@ public class pair {
 		String sql="select * from tmpuse;";
 		PreparedStatement pst=link.prepareStatement(sql);
 		result=pst.executeQuery();
-		System.out.println("------tmpuse info");
 		while(result.next()){
 			dbuser[i]=result.getString(2);
-			System.out.println(dbuser[i]);
+			//System.out.println(dbuser[i]);
 			dbtime[i]=result.getString(3);
-			System.out.println(dbtime[i]);
+			//System.out.println(dbtime[i]);
 			i++;
 		}
 		link.close();
@@ -55,7 +63,6 @@ public class pair {
 		int head,i=0,flag=0;
 		String tmp="";
 		FileReader fr=new FileReader("output.txt");
-		System.out.println("in record");
 		while((head=fr.read())!=-1){
 			if(head!=' '&&flag==0){
 				tmp+=(char)head;
@@ -66,33 +73,41 @@ public class pair {
 				flag=1;
 				//System.out.println(rctime[i]);
 			}
-			if(head!='\n'&&head!=','&&flag==1){
+			if(flag==1&&((head>='0'&&head<='9')||head=='.'||head=='-')){
 				tmp+=(char)head;
 			}
 			if(head=='\n'){
-				rclevel[i]=Integer.valueOf(tmp);
+				if(!tmp.equals("")){
+					rcheight[i]=Double.valueOf(tmp);
+				}
 				tmp="";
 				flag=0;
+				//System.out.println(rcheight[i]);
 				i++;
-				System.out.println(rclevel[i]);
 			}
 		}
 		fr.close();
 		//System.out.println(i);
 		return i;
 	}
+	public static int getlevel(double height){
+		for(int i=1;i<=9;i++){
+			if(height>=leveldata[i][0]&&height<=leveldata[i][1]){
+				return i;
+			}
+		}
+		return 1;
+	}
 	public static void match()throws IOException,SQLException,ClassNotFoundException{
 		int i,j,bufnum;
-		String usetime;
-		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");//设置日期格式
 		for(i=0;i<dblength;i++){
 			for(j=0;j<rclength;j++){
 				if(dbtime[i].equals(rctime[j])){
-					bufnum=updatebuf(dbuser[i],rclevel[j]);
+					//System.out.println(dbtime[i]);
+					bufnum=updatebuf(dbuser[i],getlevel(rcheight[j]));
 					if(bufnum!=-1){
-						usetime=df.format(new Date());
-						System.out.println(dbuser[i]+" SEND !");
-						send(9,swift,dbuser[i],usetime,i_buf[bufnum][1],i_buf[bufnum][2]);
+						System.out.println("-->"+dbuser[i]+" SEND");
+						send(9,swift,dbuser[i],rctime[j],i_buf[bufnum][1],i_buf[bufnum][2]);
 						swift++;
 					}
 				}
